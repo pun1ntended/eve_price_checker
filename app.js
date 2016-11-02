8eebb0e8b31b55b039567bad5e1f5b3457ba5dee
Steven McGrath <mcgrath.steven7@gmail.com>
2016-11-02 18:17:49 -0400

    api endpoints now function as they should

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express'),
+      path = require('path'),
+      favicon = require('serve-favicon'),
+      logger = require('morgan'),
+      cookieParser = require('cookie-parser'),
+      bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./routes/index'),
+    items = require('./routes/items');
 
 var app = express();
 
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/items', items);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/items.js
+++ routes/items.js
@@ -2,8 +2,41 @@ const express          = require('express'),
       router           = express.Router(),
       pg               = require('pg'),
       path             = require('path'),
-      connectionString = process.env.DATABASE_URL | 'postgres://localhost:5432/postgres'; //change this to production database when ready
+      conString = 'pg://localhost:5432/eve_items'; //change this to production database when ready
 
-router.get('/api/v1/items', function (req, res, next) {
-
+router.get('/', (req, res, next) => {
+  const results = [];
+  var client = new pg.Client(conString); // Get a Postgres client from the connection pool
+  client.connect();
+    // SQL Query > Select Data
+    const query = client.query('SELECT * FROM items ORDER BY typeID ASC;');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      res.send(JSON.stringify(results));
+      client.end();
+    });
 });
+router.get('/:item_id', (req, res, next) => {
+  var id = req.params.item_id;
+  const results = [];
+  var client = new pg.Client(conString); // Get a Postgres client from the connection pool
+  client.connect();
+  // SQL Query > Select Data
+  const query = client.query('SELECT * FROM items WHERE typeID = $1', [id]);
+  // Stream results back one row at a time
+  query.on('row', (row) => {
+    results.push(row);
+  });
+  // After all data is returned, close connection and return results
+  query.on('end', () => {
+    res.send(JSON.stringify(results));
+    client.end();
+  });
+});
+
+
+module.exports = router;
